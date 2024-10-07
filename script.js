1192113830131791f3ae591624f9fce6007b5795
Rohit Bej <145686961+mr-rohit-7903@users.noreply.github.com>
2024-10-07 10:47:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+//API url
 const apiUrl = 'https://programming-quotesapi.vercel.app/api/random';
 
+// required HTML 
 const quote = document.querySelector(".quote");
 const author = document.querySelector(".author");
 const newQuote = document.querySelector(".new");
 const tweet = document.querySelector(".tweet");
 
+// function to fetch data from API 
 async function fetchData() {
+
+    // try to fetch Data
     try{
         const response = await fetch(apiUrl);
         if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
@@ -13,13 +18,19 @@ async function fetchData() {
         const data = await response.json(); 
         console.log(data);
 
+        //Adding Quuote
         quote.innerHTML = data.quote;
+
+        //initializing author to blank
         author.innerHTML = '';
+
+        //adding a span element
         span = document.createElement("span");
         span.innerHTML = "-"
         author.appendChild(span);
         author.appendChild(document.createTextNode('  ' + data.author));
-        
+
+        //sharing to Twitter        
         tweet.addEventListener("click", () =>{
             //Encode the tweet text for the URL
             const encodedTweet = encodeURIComponent(data.quote);
@@ -30,15 +41,16 @@ async function fetchData() {
             //Open the Twitter URL in a new tab
             window.open(twitterUrl, '_blank');
         });
-        
+
     }catch(error){
         console.error('Error fetching the API:', error); 
     }
 }
 
+//New Quote Button 
 newQuote.addEventListener('click', () =>{
     fetchData();
 });
 
-
+//Calling function to fetch data for the first time
 fetchData();
